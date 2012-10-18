2ccbc7b280bf8d7c758b845055dfc7f185133fd9
yumu <yumu.stp+git@gmail.com>
2012-10-19 00:32:34 +0900

    rename cppFizzBuzz

--- testcpp/main.cpp
+++ testcpp/main.cpp
@@ -12,7 +12,23 @@ int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i;
+    
+    int max = atoi(argv[1]);
+    for (i = 0; i <= max; i++) {
+        if ((i % 3 == 0)&&(i % 5 == 0)) {
+           std::cout << "FizzBuzz" << std::endl;
+        }
+        else if (i % 3 == 0) {
+           std::cout << "Fizz" << std::endl;
+        }
+        else if (i % 5 == 0) {
+            std::cout << "Buzz" << std::endl;
+        }
+        else {
+            std::cout << i << std::endl;
+        }
+    }
     return 0;
 }
 
