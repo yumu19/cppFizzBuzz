aa26c50500d38675bf309faee772da3ae682c9b7
yumu <yumu.stp+git@gmail.com>
2012-10-18 23:56:26 +0900

    Initial Commit

---
+++ testcpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testcpp
+//
+//  Created by Yumura Tsubasa on 2012/10/18.
+//  Copyright (c) 2012年 Yumura Tsubasa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
